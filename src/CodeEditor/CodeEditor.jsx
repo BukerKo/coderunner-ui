d2187aabb30c6cc7ccc3abbfd0c5c43882024c87
BukerKo <buer2012@gmail.com>
2019-12-01 17:42:16 +0200

    Added CodeEditor

--- src/CodeEditor/CodeEditor.jsx
+++ src/CodeEditor/CodeEditor.jsx
@@ -1,11 +1,34 @@
 import * as React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./CodeEitor.css"
+import {TABKEY} from "../constants";
 
-export default class CodeEditor extends React.PureComponent{
+export default class CodeEditor extends React.PureComponent {
+
+    state = {
+        inputValue: "",
+        placeholder: "Write your code here"
+    };
+
+    handleChange = (event) => {
+        console.log(event);
+        this.setState({inputValue: event.target.value});
+    };
+
+    handleSubmit = (event) => {
+        alert('Submitted: ' + this.state.inputValue);
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <div>
-                Here will be textarea for code
-            </div>
+            <Form>
+                <Form.Control as={"textarea"} onChange={this.handleChange} placeholder={this.state.placeholder}/>
+                <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+                    Submit
+                </Button>
+            </Form>
         )
     }
 }
