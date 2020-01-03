7dd17c5595dba330c38e4942bd79ea9657c71b2b
nbukrieiev <nikita.bukrieiev@cengage.com>
2020-01-03 17:30:58 +0200

    Added style for errors

--- src/OutputContainer/OutputContainer.jsx
+++ src/OutputContainer/OutputContainer.jsx
@@ -3,8 +3,12 @@ import "./OutputContainer.css"
 
 export default class OutputContainer extends React.PureComponent{
     render() {
+
+        let errorClass = this.props.data.errors[0] ? 'errors ' : '';
+        let classes = errorClass + "output";
+
         return(
-            <div className={"output"}>
+            <div className={classes}>
                 {this.props.data.errors}
                 {this.props.data.output}
             </div>
