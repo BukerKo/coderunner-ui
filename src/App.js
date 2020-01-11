6368a3eec3c90c1c27e33a696d9ab05cec3618d2
Михаил Грошевой <developer.misha999777@gmail.com>
2020-01-11 20:04:47 +0200

    Trying to delete external cookies

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends React.Component {
             Cookies.remove(ACCESS_TOKEN);
             Cookies.remove(CURRENT_ROLE);
             Cookies.remove(CURRENT_USERNAME);
+            Cookies.remove("c_user");
         this.props.history.push("/");
     };
 
