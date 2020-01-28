2ec7863497bbce1ae717cf6e52313289a4da5265
nbukrieiev <nikita.bukrieiev@cengage.com>
2020-01-28 15:17:58 +0200

    fixed base url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://rue.tcomad.tk";
 export const AUTH_BASE_URL = BASE_URL + '/auth';
 export const RUN_CODE_URL = BASE_URL + "/run";
 export const SEND_CODE_URL = BASE_URL + "/sendCode";
