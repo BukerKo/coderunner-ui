4ce1a15677eb47340c341c9a9b9d734be5d14eeb
Mikhail Groshevoy <developer.misha999777@gmail.com>
2020-03-31 23:47:15 +0300

    Fixed output and errors showing

--- src/Student/OutputContainer/OutputContainer.jsx
+++ src/Student/OutputContainer/OutputContainer.jsx
@@ -3,14 +3,14 @@ import "./OutputContainer.css"
 
 export default class OutputContainer extends React.PureComponent{
     render() {
-
-        let errorClass = this.props.data.errors[0] ? 'errors ' : '';
-        let classes = errorClass + "output";
-
         return(
-            <div className={classes}>
-                {this.props.data.errors}
-                {this.props.data.output}
+            <div>
+                <div className="output">
+                    {this.props.data.output}
+                  <div className="errors">
+                    {this.props.data.errors}
+                  </div>
+                </div>
             </div>
         )
     }
