f0bce3e577e5440acaff11393a880b46e1b1ace2
Mikhail Groshevoy <developer.misha999777@gmail.com>
2020-04-03 14:38:54 +0300

    Loading task from server

--- src/Student/TaskContainer/TaskContainer.jsx
+++ src/Student/TaskContainer/TaskContainer.jsx
@@ -1,13 +1,30 @@
 import * as React from "react";
 import './TaskContainer.css'
+import {getTask} from "../../Service/RestService";
 
-export default class TaskContainer extends React.PureComponent{
+export default class TaskContainer extends React.PureComponent {
 
-    render() {
-        return (
-            <div className="task">
-                Print out "Hello World!"
-            </div>
-        )
-    }
+  state = {
+    task: ""
+  };
+
+  componentDidMount() {
+    this.props.setLoading(true);
+    getTask().then(response => {
+      this.props.setLoading(false);
+      this.setState({task: response.task});
+    }).catch((error) => {
+          this.props.setLoading(false);
+          alert(error.message || "Something went wrong, can't load task")
+        }
+    );
+  }
+
+  render() {
+    return (
+        <div className="task">
+          {this.state.task}
+        </div>
+    )
+  }
 }
